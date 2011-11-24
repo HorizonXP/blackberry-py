cfeb5caacc7709eca6c646b789ac89ed7c3b6ae7
Peter Hansen <peter@engcorp.com>
2011-11-24 16:09:03 -0500

    add higher level code to wrap screen.h stuff for minimal app

---
+++ samples/bbxpy/screen.py
@@ -0,0 +1,118 @@
+
+from ctypes import POINTER, c_int, byref, cast, sizeof
+from .wrapped.bps import *
+from .wrapped.screen import *
+
+
+def handle_screen_event(event):
+    screen_val = c_int()
+
+    screen_event = screen_event_get_event(event)
+    screen_get_event_property_iv(screen_event, SCREEN_PROPERTY_TYPE, byref(screen_val))
+
+    if screen_val == SCREEN_EVENT_MTOUCH_TOUCH:
+        print("Touch event")
+    elif screen_val == SCREEN_EVENT_MTOUCH_MOVE:
+        print("Move event")
+    elif screen_val == SCREEN_EVENT_MTOUCH_RELEASE:
+        print("Release event")
+
+
+def handle_navigator_event(event):
+    event_code = bps_event_get_code(event)
+    if event_code == NAVIGATOR_SWIPE_DOWN:
+        print("Swipe down event")
+    elif event_code == NAVIGATOR_EXIT:
+        print("Exit event")
+        shutdown = true
+
+
+def handle_event():
+    event = POINTER(bps_event_t)()
+    rc = bps_get_event(byref(event), -1)
+    assert rc == BPS_SUCCESS
+
+    if event:
+        domain = bps_event_get_domain(event)
+        if domain == navigator_get_domain():
+            handle_navigator_event(event)
+        elif domain == screen_get_domain():
+            handle_screen_event(event)
+
+def showptr(p):
+    return p.contents if p else 'NULL'
+
+
+class Screen:
+    _initialized = False
+
+    def setup(self):
+        usage = c_int(SCREEN_USAGE_NATIVE)
+
+        self.screen_ctx = screen_context_t()
+        self.screen_win = screen_window_t()
+        self.screen_buf = screen_buffer_t()
+        rect = (c_int * 4)()
+        print('rect', list(rect))
+        print('ctx', showptr(self.screen_ctx))
+        print('win', showptr(self.screen_win))
+        print('buf', showptr(self.screen_buf))
+
+        # Setup the window
+        rc = screen_create_context(byref(self.screen_ctx), 0)
+        print('screen_create_context', rc)
+        print('ctx', showptr(self.screen_ctx))
+
+        screen_create_window(byref(self.screen_win), self.screen_ctx)
+        print('win', showptr(self.screen_win))
+
+        rc = screen_set_window_property_iv(self.screen_win, SCREEN_PROPERTY_USAGE, byref(usage))
+        print('screen_set_window_property_iv', rc)
+
+        rc = screen_create_window_buffers(self.screen_win, 1)
+        print('screen_create_window_buffers', rc)
+
+        rc = screen_get_window_property_pv(self.screen_win, SCREEN_PROPERTY_RENDER_BUFFERS,
+            cast(byref(self.screen_buf), POINTER(c_void_p)))
+        print('screen_get_window_property_pv', rc)
+        print('buf', showptr(self.screen_buf))
+
+        rc = screen_get_window_property_iv(self.screen_win, SCREEN_PROPERTY_BUFFER_SIZE,
+            cast(byref(rect, 2 * sizeof(c_int)), POINTER(c_int)))
+        print('screen_get_window_property_iv', rc)
+        print('rect', list(rect))
+
+        # Fill the screen buffer with blue
+        attribs = (c_int * 3)(SCREEN_BLIT_COLOR, 0xff0000ff, SCREEN_BLIT_END)
+        rc = screen_fill(self.screen_ctx, self.screen_buf, attribs)
+        print('screen_fill', rc)
+        rc = screen_post_window(self.screen_win, self.screen_buf, 1, rect, 0)
+        print('screen_post_window', rc)
+
+        #~ # Signal bps library that navigator and screen events will be requested
+        #~ bps_initialize()
+        #~ screen_request_events(self.screen_ctx)
+        #~ navigator_request_events(0)
+
+        self._initialized = True
+
+
+    def poll(self):
+        if self._initialized:
+            # Handle user input
+            pass
+            #~ handle_event()
+
+
+    def cleanup(self):
+        # Clean up
+        #~ screen_stop_events(self.screen_ctx)
+        #~ bps_shutdown()
+        rc = screen_destroy_window(self.screen_win)
+        print('screen_destroy_window', rc)
+
+        rc = screen_destroy_context(self.screen_ctx)
+        print('screen_destroy_context', rc)
+
+        self._initialized = False
+
--- samples/scripts/script2.py
+++
@@ -1,5 +0,0 @@
-import time
-import qnx.dialog
-def run():
-    buttonOkay = qnx.dialog.make_button("OK")
-    dialog = qnx.dialog.Dialog(42, qnx.dialog.Dialog.TYPE_ALERT, buttons=[buttonOkay])
