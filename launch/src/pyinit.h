878f92d5eafc884d5f42ff1aad7ccade577868c2
Peter Hansen <peter@engcorp.com>
2011-11-24 00:59:36 -0500

    NDK 2.0 beta version with project files.  note: app exits after 30s... OS kills it for not rendering a window?

---
+++ launch/src/pyinit.h
@@ -0,0 +1,20 @@
+/*
+ * pyinit.h
+ *
+ *  Created on: 2011-11-17
+ *      Author: phansen
+ */
+
+#ifndef PYINIT_H_
+#define PYINIT_H_
+
+// initialize Python subsystem, loading library
+int py_initialize(int argc, char ** argv);
+
+// cleanup when we're done
+void py_finalize();
+
+// launch bbxmain.py
+void py_runmain();
+
+#endif /* PYINIT_H_ */
