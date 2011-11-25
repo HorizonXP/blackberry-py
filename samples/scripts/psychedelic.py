0b117d00e6445c51d51c5d1b2ef6cfb60188ff39
Peter Hansen <peter@engcorp.com>
2011-11-24 23:24:29 -0500

    flash screen random colours... missed adding this earlier

---
+++ samples/scripts/psychedelic.py
@@ -0,0 +1,17 @@
+'''Demonstrate flashing screen different colours'''
+
+import time
+import random
+
+
+def run():
+    import bbxrun
+
+    start = time.time()
+    while time.time() - start < 5:
+        colour = random.randint(0, 2**32-1)
+        bbxrun.screen.fill_screen(colour)
+        time.sleep(0.04)
+
+
+# EOF
