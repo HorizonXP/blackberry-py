e8e6d2ae8a0f611f1482b32dc0c3a5c5ffb28f28
Peter Hansen <peter@engcorp.com>
2011-11-24 23:25:13 -0500

    crude proof-of-concept/demo for event handling... works, but obviously need some Python event classes etc to make this manageable

---
+++ samples/scripts/event_test.py
@@ -0,0 +1,18 @@
+'''Demonstrate/test event handling.'''
+
+import sys
+import time
+#~ import qnx.notification as qn
+from bbxpy.wrapped.bps import virtualkeyboard_request_events
+
+
+def run():
+    import bbxrun
+    screen = bbxrun.screen
+
+    virtualkeyboard_request_events(0)
+
+    start = time.time()
+    while time.time() - start < 10:
+        screen.poll()
+        time.sleep(0.05)
