44b70a6b9cf8cecf7cf49006bddbe18afa9a3219
Peter Hansen <peter@engcorp.com>
2011-11-26 16:50:07 -0500

    include more info about displaying, including supported modes

--- samples/scripts/display_info.py
+++ samples/scripts/display_info.py
@@ -33,7 +33,7 @@ def run():
 
     for i in range(dispcount):
         disp = displays[i]
-        print('display #%d' % i, disp)
+        print('display #%d' % i) # , disp)
 
         rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_DETACHABLE, byref(intval))
         if rc: raise RuntimeError(rc)
@@ -47,9 +47,18 @@ def run():
         if rc: raise RuntimeError(rc)
         print('  num formats', intval.value)
 
+        formats = (c_int * intval.value)()
+        rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_FORMATS, formats)
+        if rc: raise RuntimeError(rc)
+        print('  formats', list(formats))
+
         rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_ROTATION, byref(intval))
         if rc: raise RuntimeError(rc)
         print('  rotation', intval.value)
+
+        rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_GAMMA, byref(intval))
+        if rc: raise RuntimeError(rc)
+        print('  gamma', intval.value)
 
         rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_MIRROR_MODE, byref(intval))
         if rc: raise RuntimeError(rc)
@@ -72,9 +81,46 @@ def run():
         if rc: raise RuntimeError(rc)
         print('  id string', charval.value)
 
+        rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_TYPE, byref(intval))
+        if rc: raise RuntimeError(rc)
+        print('  type', intval.value)
+
+        psize = (c_int * 2)()
+        rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_PHYSICAL_SIZE, cast(byref(psize), POINTER(c_int)))
+        if rc: raise RuntimeError(rc)
+        print('  physical size', psize[0], psize[1])
+
+        size = (c_int * 2)()
+        rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_SIZE, cast(byref(size), POINTER(c_int)))
+        if rc: raise RuntimeError(rc)
+        print('  resolution', size[0], size[1])
+
+        try:
+            pitch = psize[0] / size[0]
+            print('  pixel size %.2fmm (%.1fdpi)' % (pitch, 25.4/pitch))
+        except:
+            pass
+
         rc = screen_get_display_property_iv(disp, SCREEN_PROPERTY_MODE_COUNT, byref(intval))
         if rc: raise RuntimeError(rc)
         print('  num modes', intval.value)
+        modecount = intval.value
+
+        modes = (screen_display_mode_t * modecount)()
+
+        # get ref to modes
+        rc = screen_get_display_modes(disp, len(modes), modes)
+        if rc: raise RuntimeError(rc)
+
+        for j in range(modecount):
+            mode = modes[j]
+            if not mode:
+                print('    mode #%d:' % j, '(null)')
+                continue
+
+            print('    mode #%s: %s x %s @%sHz, interlaced? %s, index %s, flags %s, aspect_ratio %s/%s' %
+                (j, mode.width, mode.height, mode.refresh, mode.interlaced, mode.index, mode.flags,
+                mode.aspect_ratio[0], mode.aspect_ratio[1]))
 
     time.sleep(1)
 
