81715262c267d43fe75b4f59e1d10b4f451cbcf8
Peter Hansen <peter@engcorp.com>
2011-11-29 21:20:58 -0500

    fix some paths that were pointing to earlier stuff that was only on my own tablet

--- demo/scripts/play_sound.py
+++ demo/scripts/play_sound.py
@@ -1,7 +1,10 @@
 '''Demonstrate playing WAV file'''
 
+import os
 from qnx import audio
+
+WAV_FILE = 'app/python/scripts/zirbly.wav'
 
 def run():
     player = audio.AudioPlayer()
-    player.play('shared/documents/scripts/zirbly.wav')
+    player.play(WAV_FILE)
