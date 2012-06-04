f935836004ff46b7b57117380676ac7420d6a539
Peter Hansen <peter@engcorp.com>
2012-06-04 12:04:45 -0400

    make it even simpler

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -1,20 +1,12 @@
-import os
 import sys
 
-ENVDIR = os.path.dirname(__file__)
-sys.path.append(os.path.join(ENVDIR, 'blackberry-py'))
-
-from PySide.QtCore import QUrl
 from PySide.QtGui import QApplication
 from PySide.QtDeclarative import QDeclarativeView
 
 app = QApplication(sys.argv)
 
-# set up the QML part of things
 view = QDeclarativeView()
-qmlpath = os.path.join(ENVDIR, 'main.qml')
-view.setSource(QUrl.fromLocalFile(qmlpath))
+view.setSource('app/python/main.qml')
 view.show()
 
-# Enter Qt application main loop
-sys.exit(app.exec_())
+app.exec_()
