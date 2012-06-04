1517994cfcc0a6015a7cf5283f238c36e1cbdd72
Peter Hansen <peter@engcorp.com>
2012-06-04 00:43:32 -0400

    add standard icon

--- demo/build.cmd
+++ demo/build.cmd
@@ -11,6 +11,7 @@ echo Building for release mode
 set bbx_opts=-buildId _buildnum
 goto :build
 :build
+# call c:\bbndk-10.0.4-beta\host\win32\x86\usr\bin\blackberry-pythonpackager -package demo.bar %bbx_opts% ^
 call blackberry-pythonpackager -package demo.bar %bbx_opts% ^
     bar-descriptor.xml icon.png main.py bbxrun.py bbxpy/ scripts/ -C ../freetype-py-0.3.3 ../freetype-py-0.3.3/freetype
 endlocal
