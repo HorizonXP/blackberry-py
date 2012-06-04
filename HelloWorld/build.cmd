0738717452a1df5dfa8d332dd1ef26a3dda75c2a
Peter Hansen <peter@engcorp.com>
2012-06-04 15:08:02 -0400

    okay, icon is back in, and realized the build.cmd is really only for us to build release versions of HelloWorld.bar

--- HelloWorld/build.cmd
+++ HelloWorld/build.cmd
@@ -3,13 +3,13 @@ if not "%1" == "" goto package
 echo Specify debug token .bar file as first argument.
 goto done
 :package
-call blackberry-pythonpackager -package HelloWorld.bar -devMode ^
-    -debugToken %1 ^
+call blackberry-pythonpackager -package HelloWorld.bar ^
     -env PYTHONPATH=app/python/blackberry-py ^
     bar-descriptor.xml ^
     main.py main.qml ^
     -e ../assets/icon.png icon.png ^
     -e ../blackberry-py/ blackberry-py/ ^
-    -e ../bbpy/ blackberry-py/bbpy
+    -e ../bbpy/ blackberry-py/bbpy ^
+    -debugToken %*
 rem    -env PYTHONPATH=shared/misc/blackberry-py:shared/misc ^
 :done
