d56ea442666d27172b1d3e4985095d625a6f6d31
Peter Hansen <peter@engcorp.com>
2012-06-08 01:12:50 -0400

    use simpler approach to setting initial geometry, and try to emulate Cascades' swipeDown support

--- bbpy/app.py
+++ bbpy/app.py
@@ -1,14 +1,35 @@
 '''BlackBerry-Py base application class.'''
 
-import os
 import sys
 
-# from PySide.QtCore import Slot
+from PySide.QtCore import qDebug, Signal, Qt
 from PySide.QtGui import QApplication
 from PySide.QtDeclarative import QDeclarativeView
 
 
+class BBPyView(QDeclarativeView):
+    # signals
+    swipeDown = Signal()    # user performed swipe down from top bezel
+
+    def keyPressEvent(self, e):
+        # qDebug(':view]-keyPress {}{}'.format(e.type(), ' spont.' if e.spontaneous() else ''))
+        if e.key() == Qt.Key_Menu:
+            self.swipeDown.emit()
+            qDebug('emitted swipeDown ')
+        return super(BBPyView, self).keyPressEvent(e)
+
+
+
 class Application(QApplication):
+    # signals
+    swipeDown = Signal()    # user performed swipe down from top bezel
+
+
+    def event(self, e):
+        # qDebug(':app]-event {}{}'.format(e.type(), ' spont.' if e.spontaneous() else ''))
+        return super(Application, self).event(e)
+
+
     def __init__(self, qml=None):
         super(Application, self).__init__(sys.argv)
 
@@ -30,37 +51,24 @@ class Application(QApplication):
         w, h = self.desktop().size().toTuple()
         if self._view:
             self._view.setFixedSize(w, h)
-        # after this we get a spurious(?) onSceneRectChanged
-        # but the desktop size is still correct...
-
-
-    # @Slot()
-    def _onSceneRectChanged(self, *args):
-        '''we get this on initial startup, though the rect is (0,0,1020,157)
-        but then we also get a duplicate with 1024,600 a moment later'''
-        w, h = self.desktop().size().toTuple()
-        # this triggers the root to resize if using SizeRootObjectToView
-        # this triggers the root onWidth/HeightChanged routines
-        # this triggers onSceneResized()
-        if self._view:
-            self._view.setFixedSize(w, h)
 
 
     def create_view(self):
         # view, root, and scene all seem to get a WindowActivate/Deactivate event
         # when app goes inactive etc; view also gets an ActivationChange event
         # and FocusIn/Out events
-        self._view = v = QDeclarativeView()
+        self._view = v = BBPyView()
         v.setResizeMode(v.ResizeMode.SizeRootObjectToView)
         return v
 
 
     def run(self):
+        # resize to match current desktop
         if sys.platform == 'qnx6':
             if self._view:
-                scene = self._view.rootObject().scene()
-                scene.sceneRectChanged.connect(self._onSceneRectChanged)
+                self._view.setGeometry(self.desktop().geometry())
 
+                self._view.swipeDown.connect(self.swipeDown)
         # Enter Qt application main loop
         sys.exit(self.exec_())
 
