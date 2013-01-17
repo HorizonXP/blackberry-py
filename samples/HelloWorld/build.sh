bcbddaa5b56f380e3327a9e574f5cc244bb5bacf
Xitij Ritesh Patel <xpatel@pulsecode.ca>
2013-01-17 03:55:51 -0500

    Fix build.sh so that it points to shared for the PYTHONPATH

--- samples/HelloWorld/build.sh
+++ samples/HelloWorld/build.sh
@@ -4,7 +4,7 @@ blackberry-nativepackager -package HelloWorld.bar \
     -configuration Device-Debug \
     -devMode \
     -target bar \
-    -env PYTHONPATH=app/native \
+    -env PYTHONPATH=shared/misc/tart/python:shared/misc/HelloWorld\
     -env PYTHONDONTWRITEBYTECODE=1 \
     -arg -qml -arg shared/misc/HelloWorld/assets/main.qml \
     -arg shared/misc/tart/python/blackberry_tart.py \
